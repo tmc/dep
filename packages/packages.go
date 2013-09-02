dd2b04d1c25de25c535825e121d838317f97c41e
Marc Rene Arns <arns@stridor.net>
2013-09-02 21:26:05 +0200

    fix external exports

--- packages/packages.go
+++ packages/packages.go
@@ -1,6 +1,7 @@
 package packages
 
 import (
+	// "fmt"
 	"github.com/metakeule/exports"
 	"go/build"
 	"os"
@@ -11,14 +12,26 @@ import (
 var (
 	GOPATH       = os.Getenv("GOPATH")
 	GOROOT       = runtime.GOROOT()
+	C_Path       = path.Join(GOPATH, "src", "C")
 	PackageCache = map[string]*Package{}
 )
 
 type Package struct {
-	Path     string
-	Internal bool `json:"-"`
-	Imports  map[string]bool
-	Exports  map[string]interface{}
+	Path            string
+	Internal        bool `json:"-"`
+	Imports         map[string]bool
+	Exports         map[string]interface{}
+	ExternalExports map[string]interface{}
+}
+
+func (ø *Package) ParseExternalExports() {
+	expr := exports.SelectorExpressions(ø.Path)
+
+	for k, _ := range expr {
+		//fmt.Printf("expr: %#v\n", k)
+		ø.ExternalExports[k[0]+"#"+k[1]] = Get(k[0]).Exports[k[1]]
+	}
+
 }
 
 func (ø *Package) ParseExports() {
@@ -27,6 +40,9 @@ func (ø *Package) ParseExports() {
 
 func (ø *Package) ParseImports() {
 	dir := path.Join(GOPATH, "src", ø.Path)
+	if dir == C_Path {
+		return
+	}
 	pkg, err := build.Default.ImportDir(dir, build.AllowBinary)
 	if err != nil {
 		dir := path.Join(GOROOT, "src", "pkg", ø.Path)
@@ -51,13 +67,18 @@ func (ø *Package) ParseImports() {
 }
 
 func Get(path string) (ø *Package) {
+	if path == C_Path {
+		panic("can't get C path: " + path)
+	}
 	if p, ok := PackageCache[path]; ok {
 		return p
 	}
 	ø = &Package{Path: path}
 	ø.Imports = map[string]bool{}
+	ø.ExternalExports = map[string]interface{}{}
 	PackageCache[path] = ø
 	ø.ParseImports()
 	ø.ParseExports()
+	ø.ParseExternalExports()
 	return
 }
